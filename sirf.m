557349c26506200e4289bb836968d88b2759c294
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2023-06-26 17:43:08 -0400

    Update sirf.m

--- sirf.m
+++ sirf.m
@@ -1,5 +1,5 @@
 %## Read the FIS structure from a file.
-%pkg load fuzzy-logic-toolkit
+pkg load fuzzy-logic-toolkit
 fis = readfis ('sirf.fis');
 %## Plot the input and output membership functions.
 %plotmf (fis, 'input', 1);
@@ -10,25 +10,21 @@ fis = readfis ('sirf.fis');
 %gensurf(fis, [1 2]);
 %## Plot the Tip and Check + Tip as functions of Food-Quality
 %gensurf(fis,);
-%data=load('inputs.mat');
-RS =  evalfis([69.642 68.389 49.512],fis,101);
-disp(['RS=' num2str(RS)]);
-
-
+data=load('inputs.mat');
 %aux = [];
-%for i=1:50
-%  if data.inputs(i,1) < 40 && data.inputs(i,2) < 40 && data.inputs(i,3) < 40
-%    fis.defuzzificationMethod = 'mom';
-%    out = evalfis([data.inputs(i,1), data.inputs(i,2), data.inputs(i,3)],fis,101);
-%    aux = [aux; out];
-%  else
-%    fis.defuzzificationMethod = 'centroid';
-%    out = evalfis([data.inputs(i,1), data.inputs(i,2), data.inputs(i,3)],fis,101);
-%    aux = [aux; out];
-%  end
+for i=1:5000
+  if data.inputs(i,1) < 40 && data.inputs(i,2) < 40 && data.inputs(i,3) < 40
+    fis.defuzzificationMethod = 'mom';
+    out = evalfis([data.inputs(i,1), data.inputs(i,2), data.inputs(i,3)],fis,101);
+    aux = [aux; out];
+  else
+    fis.defuzzificationMethod = 'centroid';
+    out = evalfis([data.inputs(i,1), data.inputs(i,2), data.inputs(i,3)],fis,101);
+    aux = [aux; out];
+  end
 
  % disp(out);
-%end
+end
 
 %disp('-----Octave, save -V7 O_MOM.mat aux-------------------------------------------------------------------');
 %disp(aux)
