366bb16461635db40d1348a0146f20ff1a81c0c3
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2024-07-10 15:39:58 -0400

    Update run.sh

--- INSTALL/run.sh
+++ INSTALL/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm $PWD/sr.txt
 mv ~/Downloads/sr.txt .
-cat sr.txt | awk -F"," '{print "Security Practices: "$1 " %"}'> result.txt
-cat sr.txt | awk -F"," '{print "Software:           "$2 " %"}'>> result.txt
-cat sr.txt | awk -F"," '{print "People:             "$3 " %"}'>> result.txt
+cat sr.txt | awk -F"," '{printf "Security Practices: %.3f%\n", $1}'> result.txt
+cat sr.txt | awk -F"," '{printf "Software:           %.3f%\n", $2}'>> result.txt
+cat sr.txt | awk -F"," '{printf "People:             %.3f%\n", $3}'>> result.txt
 ./octave.m > sra.txt
- 
